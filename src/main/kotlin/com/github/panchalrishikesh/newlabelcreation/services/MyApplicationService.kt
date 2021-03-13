640956a00f71775d8dea4de7a70e46422401cabc
GitHub Action <action@github.com>
2021-03-13 08:36:58 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/panchalrishikesh/newlabelcreation/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.panchalrishikesh.newlabelcreation.services
+
+import com.github.panchalrishikesh.newlabelcreation.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/panchalrishikesh/newlabelcreation/services/MyProjectService.kt
+++ src/main/kotlin/com/github/panchalrishikesh/newlabelcreation/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.panchalrishikesh.newlabelcreation.services
 
+import com.github.panchalrishikesh.newlabelcreation.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
